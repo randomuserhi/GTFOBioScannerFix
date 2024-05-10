91c9db14aec199629ec52658096e1c47ca69f585
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-05-10 23:00:39 +0100

    initial

---
+++ BioScannerFix/BioScannerFix/Patches/Fix.cs
@@ -0,0 +1,13 @@
+﻿using Enemies;
+using HarmonyLib;
+
+namespace BioScannerFix {
+    [HarmonyPatch]
+    internal static class Fix {
+        [HarmonyPatch(typeof(EnemyAgent), nameof(EnemyAgent.ScannerData), MethodType.Setter)]
+        [HarmonyPostfix]
+        private static void Postfix_Set_ScannerData(EnemyAgent __instance) {
+            __instance.m_hasDirtyScannerColor = true;
+        }
+    }
+}
